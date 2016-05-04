ab4835be72d705fcd8eaff07ecb7f87f5ae935e0
emersion <contact@emersion.fr>
2016-05-04 18:34:27 +0200

    Fixes typo

--- client/cmd_any.go
+++ client/cmd_any.go
@@ -9,7 +9,7 @@ import (
 
 // Request a listing of capabilities that the server supports. Capabilities are
 // often returned by the server with the greeting or with the STARTTLS and LOGIN
-// responses, so usally explicitely requesting capabilities isn't needed.
+// responses, so usually explicitely requesting capabilities isn't needed.
 func (c *Client) Capability() (caps map[string]bool, err error) {
 	cmd := &commands.Capability{}
 
