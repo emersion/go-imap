2cc5885f137db61b7acba947a4e2ee5e3e2829e2
emersion <contact@emersion.fr>
2017-01-08 21:32:39 +0100

    backendutil: adds tests for MatchFlags

--- backend/backendutil/search_test.go
+++ backend/backendutil/search_test.go
@@ -6,6 +6,57 @@ import (
 	"github.com/emersion/go-imap"
 )
 
+var flagsTests = []struct{
+	flags []string
+	criteria *imap.SearchCriteria
+	res bool
+}{
+	{
+		flags: []string{imap.SeenFlag},
+		criteria: &imap.SearchCriteria{
+			Seen: true,
+			Unflagged: true,
+		},
+		res: true,
+	},
+	{
+		flags: []string{imap.SeenFlag, imap.RecentFlag},
+		criteria: &imap.SearchCriteria{New: true},
+		res: false,
+	},
+	{
+		flags: []string{imap.RecentFlag},
+		criteria: &imap.SearchCriteria{New: true},
+		res: true,
+	},
+	{
+		flags: []string{imap.SeenFlag},
+		criteria: &imap.SearchCriteria{
+			Not: &imap.SearchCriteria{Unseen: true},
+		},
+		res: true,
+	},
+	{
+		flags: []string{imap.RecentFlag},
+		criteria: &imap.SearchCriteria{
+			Not: &imap.SearchCriteria{Unseen: true},
+		},
+		res: false,
+	},
+}
+
+func TestMatchFlags(t *testing.T) {
+	for i, test := range flagsTests {
+		ok := MatchFlags(test.flags, test.criteria)
+		if test.res && !ok {
+			t.Errorf("Expected #%v to match search criteria", i)
+		}
+		if !test.res && ok {
+			t.Errorf("Expected #%v not to match search criteria", i)
+		}
+	}
+}
+
 func TestMatchSeqNumAndUid(t *testing.T) {
 	seqNum := uint32(42)
 	uid := uint32(69)
