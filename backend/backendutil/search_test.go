5d25459c6350b21f5e40f1ea74f780dfe8714d39
fox.cpp <fox.cpp@disroot.org>
2019-05-04 11:27:18 +0300

    backendutil: Merge Match* functions

--- backend/backendutil/search_test.go
+++ backend/backendutil/search_test.go
@@ -10,8 +10,14 @@ import (
 	"github.com/emersion/go-message"
 )
 
+var testInternalDate = time.Unix(1483997966, 0)
+
 var matchTests = []struct {
 	criteria *imap.SearchCriteria
+	seqNum   uint32
+	uid      uint32
+	date     time.Time
+	flags    []string
 	res      bool
 }{
 	{
@@ -105,34 +111,6 @@ var matchTests = []struct {
 		},
 		res: false,
 	},
-}
-
-func TestMatch(t *testing.T) {
-	for i, test := range matchTests {
-		e, err := message.Read(strings.NewReader(testMailString))
-		if err != nil {
-			t.Fatal("Expected no error while reading entity, got:", err)
-		}
-
-		ok, err := Match(e, test.criteria)
-		if err != nil {
-			t.Fatal("Expected no error while matching entity, got:", err)
-		}
-
-		if test.res && !ok {
-			t.Errorf("Expected #%v to match search criteria", i+1)
-		}
-		if !test.res && ok {
-			t.Errorf("Expected #%v not to match search criteria", i+1)
-		}
-	}
-}
-
-var flagsTests = []struct {
-	flags    []string
-	criteria *imap.SearchCriteria
-	res      bool
-}{
 	{
 		flags: []string{imap.SeenFlag},
 		criteria: &imap.SearchCriteria{
@@ -176,89 +154,161 @@ var flagsTests = []struct {
 		},
 		res: false,
 	},
+	{
+		seqNum: 42,
+		uid:    69,
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Uid: new(imap.SeqSet),
+					Not: []*imap.SearchCriteria{{SeqNum: new(imap.SeqSet)}},
+				},
+				{
+					SeqNum: new(imap.SeqSet),
+				},
+			}},
+		},
+		res: false,
+	},
+	{
+		seqNum: 42,
+		uid:    69,
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Uid: &imap.SeqSet{Set: []imap.Seq{{69, 69}}},
+					Not: []*imap.SearchCriteria{{SeqNum: new(imap.SeqSet)}},
+				},
+				{
+					SeqNum: new(imap.SeqSet),
+				},
+			}},
+		},
+		res: true,
+	},
+	{
+		seqNum: 42,
+		uid:    69,
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Uid: &imap.SeqSet{Set: []imap.Seq{{69, 69}}},
+					Not: []*imap.SearchCriteria{{
+						SeqNum: &imap.SeqSet{Set: []imap.Seq{imap.Seq{42, 42}}},
+					}},
+				},
+				{
+					SeqNum: new(imap.SeqSet),
+				},
+			}},
+		},
+		res: false,
+	},
+	{
+		seqNum: 42,
+		uid:    69,
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Uid: &imap.SeqSet{Set: []imap.Seq{{69, 69}}},
+					Not: []*imap.SearchCriteria{{
+						SeqNum: &imap.SeqSet{Set: []imap.Seq{{42, 42}}},
+					}},
+				},
+				{
+					SeqNum: &imap.SeqSet{Set: []imap.Seq{{42, 42}}},
+				},
+			}},
+		},
+		res: true,
+	},
+	{
+		date: testInternalDate,
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Since: testInternalDate.Add(48 * time.Hour),
+					Not: []*imap.SearchCriteria{{
+						Since: testInternalDate.Add(48 * time.Hour),
+					}},
+				},
+				{
+					Before: testInternalDate.Add(-48 * time.Hour),
+				},
+			}},
+		},
+		res: false,
+	},
+	{
+		date: testInternalDate,
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Since: testInternalDate.Add(-48 * time.Hour),
+					Not: []*imap.SearchCriteria{{
+						Since: testInternalDate.Add(48 * time.Hour),
+					}},
+				},
+				{
+					Before: testInternalDate.Add(-48 * time.Hour),
+				},
+			}},
+		},
+		res: true,
+	},
+	{
+		date: testInternalDate,
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Since: testInternalDate.Add(-48 * time.Hour),
+					Not: []*imap.SearchCriteria{{
+						Since: testInternalDate.Add(-48 * time.Hour),
+					}},
+				},
+				{
+					Before: testInternalDate.Add(-48 * time.Hour),
+				},
+			}},
+		},
+		res: false,
+	},
+	{
+		date: testInternalDate,
+		criteria: &imap.SearchCriteria{
+			Or: [][2]*imap.SearchCriteria{{
+				{
+					Since: testInternalDate.Add(-48 * time.Hour),
+					Not: []*imap.SearchCriteria{{
+						Since: testInternalDate.Add(-48 * time.Hour),
+					}},
+				},
+				{
+					Before: testInternalDate.Add(48 * time.Hour),
+				},
+			}},
+		},
+		res: true,
+	},
 }
 
-func TestMatchFlags(t *testing.T) {
-	for i, test := range flagsTests {
-		ok := MatchFlags(test.flags, test.criteria)
+func TestMatch(t *testing.T) {
+	for i, test := range matchTests {
+		e, err := message.Read(strings.NewReader(testMailString))
+		if err != nil {
+			t.Fatal("Expected no error while reading entity, got:", err)
+		}
+
+		ok, err := Match(e, test.seqNum, test.uid, test.date, test.flags, test.criteria)
+		if err != nil {
+			t.Fatal("Expected no error while matching entity, got:", err)
+		}
+
 		if test.res && !ok {
 			t.Errorf("Expected #%v to match search criteria", i+1)
 		}
 		if !test.res && ok {
 			t.Errorf("Expected #%v not to match search criteria", i+1)
 		}
-	}
-}
-
-func TestMatchSeqNumAndUid(t *testing.T) {
-	seqNum := uint32(42)
-	uid := uint32(69)
-
-	c := &imap.SearchCriteria{
-		Or: [][2]*imap.SearchCriteria{{
-			{
-				Uid: new(imap.SeqSet),
-				Not: []*imap.SearchCriteria{{SeqNum: new(imap.SeqSet)}},
-			},
-			{
-				SeqNum: new(imap.SeqSet),
-			},
-		}},
-	}
-
-	if MatchSeqNumAndUid(seqNum, uid, c) {
-		t.Error("Expected not to match criteria")
-	}
-
-	c.Or[0][0].Uid.AddNum(uid)
-	if !MatchSeqNumAndUid(seqNum, uid, c) {
-		t.Error("Expected to match criteria")
-	}
-
-	c.Or[0][0].Not[0].SeqNum.AddNum(seqNum)
-	if MatchSeqNumAndUid(seqNum, uid, c) {
-		t.Error("Expected not to match criteria")
-	}
-
-	c.Or[0][1].SeqNum.AddNum(seqNum)
-	if !MatchSeqNumAndUid(seqNum, uid, c) {
-		t.Error("Expected to match criteria")
-	}
-}
-
-func TestMatchDate(t *testing.T) {
-	date := time.Unix(1483997966, 0)
-
-	c := &imap.SearchCriteria{
-		Or: [][2]*imap.SearchCriteria{{
-			{
-				Since: date.Add(48 * time.Hour),
-				Not: []*imap.SearchCriteria{{
-					Since: date.Add(48 * time.Hour),
-				}},
-			},
-			{
-				Before: date.Add(-48 * time.Hour),
-			},
-		}},
-	}
-
-	if MatchDate(date, c) {
-		t.Error("Expected not to match criteria")
-	}
-
-	c.Or[0][0].Since = date.Add(-48 * time.Hour)
-	if !MatchDate(date, c) {
-		t.Error("Expected to match criteria")
-	}
-
-	c.Or[0][0].Not[0].Since = date.Add(-48 * time.Hour)
-	if MatchDate(date, c) {
-		t.Error("Expected not to match criteria")
-	}
-
-	c.Or[0][1].Before = date.Add(48 * time.Hour)
-	if !MatchDate(date, c) {
-		t.Error("Expected to match criteria")
 	}
 }
