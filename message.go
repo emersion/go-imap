d5fd5481d22559ff81b3270593a26bf5ee563f5e
GZxiaoBinHuang <xiao_bin_huang@163.com>
2020-01-09 23:38:27 +0800

    fix rfc822XXX issue

--- message.go
+++ message.go
@@ -463,7 +463,10 @@ func (section *BodySectionName) resp() *BodySectionName {
 	if len(resp.Partial) == 2 {
 		resp.Partial = []int{resp.Partial[0]}
 	}
-	resp.value = ""
+	// resp.value = ""
+    if !strings.HasPrefix(string(resp.value), string(FetchRFC822)) {
+        resp.value = ""
+    }
 	return &resp
 }
 
