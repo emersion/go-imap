10541c820dbbdf622cc7b83ba700b56f58f24a7a
James Houlahan <james.houlahan@protonmail.com>
2020-07-07 12:53:17 +0200

    test: remove dependency on testify

--- responses/list_test.go
+++ responses/list_test.go
@@ -5,32 +5,40 @@ import (
 	"testing"
 
 	"github.com/emersion/go-imap"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestListSlashDelimiter(t *testing.T) {
 	mbox := &imap.MailboxInfo{}
 
-	require.NoError(t, mbox.Parse([]interface{}{
+	if err := mbox.Parse([]interface{}{
 		[]interface{}{"\\Unseen"},
 		"/",
 		"INBOX",
-	}))
+	}); err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
 
-	assert.Equal(t, `* LIST (\Unseen) "/" INBOX`+"\r\n", getListResponse(t, mbox))
+	if response := getListResponse(t, mbox); response != `* LIST (\Unseen) "/" INBOX`+"\r\n" {
+		t.Error("Unexpected response:", response)
+	}
 }
 
 func TestListNILDelimiter(t *testing.T) {
 	mbox := &imap.MailboxInfo{}
 
-	require.NoError(t, mbox.Parse([]interface{}{
+	if err := mbox.Parse([]interface{}{
 		[]interface{}{"\\Unseen"},
 		nil,
 		"INBOX",
-	}))
+	}); err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
 
-	assert.Equal(t, `* LIST (\Unseen) NIL INBOX`+"\r\n", getListResponse(t, mbox))
+	if response := getListResponse(t, mbox); response != `* LIST (\Unseen) NIL INBOX`+"\r\n" {
+		t.Error("Unexpected response:", response)
+	}
 }
 
 func newListResponse(mbox *imap.MailboxInfo) (l *List) {
@@ -48,7 +56,10 @@ func getListResponse(t *testing.T, mbox *imap.MailboxInfo) string {
 	b := &bytes.Buffer{}
 	w := imap.NewWriter(b)
 
-	require.NoError(t, newListResponse(mbox).WriteTo(w))
+	if err := newListResponse(mbox).WriteTo(w); err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
 
 	return b.String()
 }
