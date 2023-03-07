6e153220f4a22e4ccfeb84f8adc48b5022e18ec6
Simon Ser <contact@emersion.fr>
2023-03-07 20:36:51 +0100

    imapwire: add support for encoding literals

--- internal/imapwire/encoder.go
+++ internal/imapwire/encoder.go
@@ -2,12 +2,16 @@ package imapwire
 
 import (
 	"bufio"
+	"fmt"
+	"io"
+	"strconv"
 	"strings"
 )
 
 type Encoder struct {
-	w   *bufio.Writer
-	err error
+	w       *bufio.Writer
+	err     error
+	literal bool
 }
 
 func NewEncoder(w *bufio.Writer) *Encoder {
@@ -16,6 +20,10 @@ func NewEncoder(w *bufio.Writer) *Encoder {
 
 func (enc *Encoder) writeString(s string) *Encoder {
 	if enc.err != nil {
+		return enc
+	}
+	if enc.literal {
+		enc.err = fmt.Errorf("imapwire: cannot encode while a literal is open")
 		return enc
 	}
 	if _, err := enc.w.WriteString(s); err != nil {
@@ -54,4 +62,63 @@ func (enc *Encoder) String(s string) *Encoder {
 	}
 	sb.WriteByte('"')
 	return enc.writeString(sb.String())
+}
+
+func (enc *Encoder) Literal(size int64, sync <-chan struct{}) io.WriteCloser {
+	// TODO: literal8
+	enc.writeString("{")
+	enc.writeString(strconv.FormatInt(size, 10))
+	if sync == nil {
+		enc.writeString("+")
+	}
+	enc.writeString("}")
+
+	if sync == nil {
+		enc.writeString("\r\n")
+	} else {
+		if err := enc.CRLF(); err != nil {
+			return errorWriter{err}
+		}
+		<-sync
+	}
+
+	enc.literal = true
+	return &literalWriter{
+		enc: enc,
+		n:   size,
+	}
+}
+
+type errorWriter struct {
+	err error
+}
+
+func (ew errorWriter) Write(b []byte) (int, error) {
+	return 0, ew.err
+}
+
+func (ew errorWriter) Close() error {
+	return ew.err
+}
+
+type literalWriter struct {
+	enc *Encoder
+	n   int64
+}
+
+func (lw *literalWriter) Write(b []byte) (int, error) {
+	if lw.n-int64(len(b)) < 0 {
+		return 0, fmt.Errorf("wrote too many bytes in literal")
+	}
+	n, err := lw.enc.w.Write(b)
+	lw.n -= int64(n)
+	return n, err
+}
+
+func (lw *literalWriter) Close() error {
+	lw.enc.literal = false
+	if lw.n != 0 {
+		return fmt.Errorf("wrote too few bytes in literal (%v remaining)", lw.n)
+	}
+	return nil
 }
