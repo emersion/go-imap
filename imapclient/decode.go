f2fa78a206d12e2aa3115ac419120c3a716c57c0
Simon Ser <contact@emersion.fr>
2023-03-08 11:43:16 +0100

    imapclient: add FETCH

--- imapclient/decode.go
+++ imapclient/decode.go
@@ -2,6 +2,7 @@ package imapclient
 
 import (
 	"fmt"
+	"io"
 
 	"github.com/emersion/go-imap/v2/internal/imapwire"
 )
@@ -56,4 +57,86 @@ func readFlag(dec *imapwire.Decoder) (string, error) {
 		name = "\\" + name
 	}
 	return name, nil
+}
+
+func readMsgAtt(dec *imapwire.Decoder, cmd *FetchCommand) error {
+	if !dec.ExpectSpecial('(') {
+		return dec.Err()
+	}
+
+	items := make(chan *FetchItemData, 32)
+	defer close(items)
+	msg := &FetchMessageData{items: items}
+
+	if cmd != nil {
+		cmd.msgs <- msg
+	} else {
+		defer msg.discard()
+	}
+
+	for {
+		var attName string
+		if !dec.ExpectAtom(&attName) {
+			return dec.Err()
+		}
+
+		// TODO: FLAGS, ENVELOPE, INTERNALDATE, RFC822.SIZE, BODY,
+		// BODYSTRUCTURE, BINARY section, BINARY.SIZE section, UID
+		var (
+			item *FetchItemData
+			done chan struct{}
+		)
+		switch attName {
+		case "BODY[":
+			// TODO: section ["<" number ">"]
+			if !dec.ExpectSpecial(']') || !dec.ExpectSP() {
+				return dec.Err()
+			}
+
+			lit, _, ok := dec.ExpectNString()
+			if !ok {
+				return dec.Err()
+			}
+
+			done = make(chan struct{})
+			item = &FetchItemData{
+				Name: "BODY[]",
+				Literal: &fetchLiteralReader{
+					LiteralReader: lit,
+					ch:            done,
+				},
+			}
+		default:
+			return fmt.Errorf("unsupported msg-att name: %q", attName)
+		}
+
+		items <- item
+		if done != nil {
+			<-done
+		}
+
+		if !dec.SP() {
+			break
+		}
+	}
+
+	if !dec.ExpectSpecial(')') {
+		return dec.Err()
+	}
+
+	return nil
+}
+
+type fetchLiteralReader struct {
+	*imapwire.LiteralReader
+	ch chan<- struct{}
+}
+
+func (lit *fetchLiteralReader) Read(b []byte) (int, error) {
+	n, err := lit.LiteralReader.Read(b)
+	if err == io.EOF && lit.ch != nil {
+		close(lit.ch)
+		lit.ch = nil
+	}
+	return n, err
 }
