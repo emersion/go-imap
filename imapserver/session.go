f3c4755be1bd4efb0656b0d9819650ee6a170768
Simon Ser <contact@emersion.fr>
2023-03-24 15:14:34 +0100

    imapserver: add ErrAuthFailed

--- imapserver/session.go
+++ imapserver/session.go
@@ -1,5 +1,18 @@
 package imapserver
 
+import (
+	"github.com/emersion/go-imap/v2"
+)
+
+var errAuthFailed = &imap.Error{
+	Type: imap.StatusResponseTypeNo,
+	Code: imap.ResponseCodeAuthenticationFailed,
+	Text: "Authentication failed",
+}
+
+// ErrAuthFailed is returned by Session.Login on authentication failure.
+var ErrAuthFailed = errAuthFailed
+
 // Session is an IMAP session.
 type Session interface {
 	Close() error
