bdd70aeec880a4d690a975dfbcc456a0f19d57ab
Simon Ser <contact@emersion.fr>
2023-03-27 19:25:43 +0200

    imapserver: send CAPABILITY code in greeting

--- imapserver/capability.go
+++ imapserver/capability.go
@@ -10,6 +10,16 @@ func (c *conn) handleCapability(dec *imapwire.Decoder) error {
 		return dec.Err()
 	}
 
+	enc := newResponseEncoder(c)
+	defer enc.end()
+	enc.Atom("*").SP().Atom("CAPABILITY")
+	for _, c := range c.availableCaps() {
+		enc.SP().Atom(string(c))
+	}
+	return enc.CRLF()
+}
+
+func (c *conn) availableCaps() []imap.Cap {
 	caps := []imap.Cap{imap.CapIMAP4rev2}
 	if c.canStartTLS() {
 		caps = append(caps, imap.CapStartTLS)
@@ -19,12 +29,5 @@ func (c *conn) handleCapability(dec *imapwire.Decoder) error {
 	} else if c.state == imap.ConnStateNotAuthenticated {
 		caps = append(caps, imap.CapLoginDisabled)
 	}
-
-	enc := newResponseEncoder(c)
-	defer enc.end()
-	enc.Atom("*").SP().Atom("CAPABILITY")
-	for _, c := range caps {
-		enc.SP().Atom(string(c))
-	}
-	return enc.CRLF()
+	return caps
 }
