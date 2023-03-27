c62fdcc5e3e182a40a79a9d514c68e3dc112b77e
Simon Ser <contact@emersion.fr>
2023-03-27 14:39:08 +0200

    imapserver: move CAPABILITY, IDLE and LOGIN handlers to separate files

---
+++ imapserver/idle.go
@@ -0,0 +1,32 @@
+package imapserver
+
+import (
+	"github.com/emersion/go-imap/v2"
+	"github.com/emersion/go-imap/v2/internal/imapwire"
+)
+
+func (c *conn) handleIdle(dec *imapwire.Decoder) error {
+	if !dec.ExpectCRLF() {
+		return dec.Err()
+	}
+
+	if err := c.checkState(imap.ConnStateAuthenticated); err != nil {
+		return err
+	}
+
+	enc := newResponseEncoder(c)
+	defer enc.end()
+
+	if err := writeContReq(enc.Encoder, "idling"); err != nil {
+		return err
+	}
+
+	line, isPrefix, err := c.br.ReadLine()
+	if err != nil {
+		return err
+	} else if isPrefix || string(line) != "DONE" {
+		return newClientBugError("Syntax error: expected DONE to end IDLE command")
+	}
+
+	return nil
+}
---
+++ imapserver/login.go
@@ -0,0 +1,28 @@
+package imapserver
+
+import (
+	"github.com/emersion/go-imap/v2"
+	"github.com/emersion/go-imap/v2/internal/imapwire"
+)
+
+func (c *conn) handleLogin(dec *imapwire.Decoder) error {
+	var username, password string
+	if !dec.ExpectSP() || !dec.ExpectAString(&username) || !dec.ExpectSP() || !dec.ExpectAString(&password) || !dec.ExpectCRLF() {
+		return dec.Err()
+	}
+	if err := c.checkState(imap.ConnStateNotAuthenticated); err != nil {
+		return err
+	}
+	if !c.canAuth() {
+		return &imap.Error{
+			Type: imap.StatusResponseTypeNo,
+			Code: imap.ResponseCodePrivacyRequired,
+			Text: "TLS is required to authenticate",
+		}
+	}
+	if err := c.session.Login(username, password); err != nil {
+		return err
+	}
+	c.state = imap.ConnStateAuthenticated
+	return nil
+}
