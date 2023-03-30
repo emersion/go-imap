9dbec83e021e2626747137929a7d30d2da7f259c
Simon Ser <contact@emersion.fr>
2023-03-30 16:45:28 +0200

    imapserver: add MailboxTracker

---
+++ imapserver/tracker.go
@@ -0,0 +1,197 @@
+package imapserver
+
+import (
+	"fmt"
+	"sync"
+)
+
+// MailboxTracker tracks the state of a mailbox.
+//
+// A mailbox can have multiple sessions listening for updates. Each session has
+// its own view of the mailbox, because IMAP clients asynchronously receive
+// mailbox updates.
+type MailboxTracker struct {
+	mutex       sync.Mutex
+	numMessages uint32
+	sessions    map[*SessionTracker]struct{}
+}
+
+// NewMailboxTracker creates a new mailbox tracker.
+func NewMailboxTracker(numMessages uint32) *MailboxTracker {
+	return &MailboxTracker{
+		numMessages: numMessages,
+		sessions:    make(map[*SessionTracker]struct{}),
+	}
+}
+
+// NewSession creates a new session tracker for the mailbox.
+//
+// The caller must call SessionTracker.Close once they are done with the
+// session.
+func (t *MailboxTracker) NewSession() *SessionTracker {
+	st := &SessionTracker{mailbox: t}
+	t.mutex.Lock()
+	t.sessions[st] = struct{}{}
+	t.mutex.Unlock()
+	return st
+}
+
+func (t *MailboxTracker) queueUpdate(update *trackerUpdate) {
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+
+	if update.expunge != 0 && update.expunge > t.numMessages {
+		panic(fmt.Errorf("imapserver: expunge sequence number (%v) out of range (%v messages in mailbox)", update.expunge, t.numMessages))
+	}
+	if update.numMessages != 0 && update.numMessages < t.numMessages {
+		panic(fmt.Errorf("imapserver: cannot decrease mailbox number of messages from %v to %v", t.numMessages, update.numMessages))
+	}
+
+	for st := range t.sessions {
+		st.queueUpdate(update)
+	}
+
+	switch {
+	case update.expunge != 0:
+		t.numMessages--
+	case update.numMessages != 0:
+		t.numMessages = update.numMessages
+	}
+}
+
+// QueueExpunge queues a new EXPUNGE update.
+func (t *MailboxTracker) QueueExpunge(seqNum uint32) {
+	if seqNum == 0 {
+		panic("imapserver: invalid expunge message sequence number")
+	}
+	t.queueUpdate(&trackerUpdate{expunge: seqNum})
+}
+
+// QueueNumMessages queues a new EXISTS update.
+func (t *MailboxTracker) QueueNumMessages(n uint32) {
+	// TODO: merge consecutive NumMessages updates
+	t.queueUpdate(&trackerUpdate{numMessages: n})
+}
+
+type trackerUpdate struct {
+	expunge     uint32
+	numMessages uint32
+}
+
+// SessionTracker tracks the state of a mailbox for an IMAP client.
+type SessionTracker struct {
+	mailbox *MailboxTracker
+
+	mutex sync.Mutex
+	queue []trackerUpdate
+}
+
+// Close unregisters the session.
+func (t *SessionTracker) Close() {
+	t.mailbox.mutex.Lock()
+	delete(t.mailbox.sessions, t)
+	t.mailbox.mutex.Unlock()
+	t.mailbox = nil
+}
+
+func (t *SessionTracker) queueUpdate(update *trackerUpdate) {
+	t.mutex.Lock()
+	t.queue = append(t.queue, *update)
+	t.mutex.Unlock()
+}
+
+// Poll dequeues pending mailbox updates for this session.
+func (t *SessionTracker) Poll(w *UpdateWriter, allowExpunge bool) error {
+	var updates []trackerUpdate
+	t.mutex.Lock()
+	if allowExpunge {
+		updates = t.queue
+		t.queue = nil
+	} else {
+		stopIndex := -1
+		for i, update := range t.queue {
+			if update.expunge != 0 {
+				stopIndex = i
+				break
+			}
+			updates = append(updates, update)
+		}
+		if stopIndex >= 0 {
+			t.queue = t.queue[stopIndex:]
+		} else {
+			t.queue = nil
+		}
+	}
+	t.mutex.Unlock()
+
+	for _, update := range updates {
+		var err error
+		switch {
+		case update.expunge != 0:
+			err = w.WriteExpunge(update.expunge)
+		case update.numMessages != 0:
+			err = w.WriteNumMessages(update.numMessages)
+		default:
+			panic(fmt.Errorf("imapserver: unknown tracker update %#v", update))
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// DecodeSeqNum converts a message sequence number from the client view to the
+// server view.
+//
+// Zero is returned if the message doesn't exist from the server point-of-view.
+func (t *SessionTracker) DecodeSeqNum(seqNum uint32) uint32 {
+	if seqNum == 0 {
+		return 0
+	}
+
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+
+	for _, update := range t.queue {
+		if update.expunge == 0 {
+			continue
+		}
+		if seqNum == update.expunge {
+			return 0
+		} else if seqNum > update.expunge {
+			seqNum--
+		}
+	}
+
+	return seqNum
+}
+
+// EncodeSeqNum converts a message sequence number from the server view to the
+// client view.
+//
+// Zero is returned if the message doesn't exist from the client point-of-view.
+func (t *SessionTracker) EncodeSeqNum(seqNum uint32) uint32 {
+	if seqNum == 0 {
+		return 0
+	}
+
+	t.mutex.Lock()
+	defer t.mutex.Unlock()
+
+	for i := len(t.queue) - 1; i >= 0; i-- {
+		update := t.queue[i]
+		// TODO: this doesn't handle increments > 1
+		if update.numMessages != 0 && seqNum == update.numMessages {
+			return 0
+		}
+		if update.expunge != 0 {
+			if seqNum == update.expunge {
+				return 0
+			} else if seqNum > update.expunge {
+				seqNum++
+			}
+		}
+	}
+	return seqNum
+}
---
+++ imapserver/tracker_test.go
@@ -0,0 +1,119 @@
+package imapserver_test
+
+import (
+	"testing"
+
+	"github.com/emersion/go-imap/v2/imapserver"
+)
+
+type trackerUpdate struct {
+	expunge     uint32
+	numMessages uint32
+}
+
+var sessionTrackerSeqNumTests = []struct {
+	name                       string
+	pending                    []trackerUpdate
+	clientSeqNum, serverSeqNum uint32
+}{
+	{
+		name:         "noop",
+		pending:      nil,
+		clientSeqNum: 42,
+		serverSeqNum: 42,
+	},
+	{
+		name:         "expunge_eq",
+		pending:      []trackerUpdate{{expunge: 20}},
+		clientSeqNum: 20,
+		serverSeqNum: 0,
+	},
+	{
+		name:         "expunge_lt",
+		pending:      []trackerUpdate{{expunge: 20}},
+		clientSeqNum: 10,
+		serverSeqNum: 10,
+	},
+	{
+		name:         "expunge_gt",
+		pending:      []trackerUpdate{{expunge: 10}},
+		clientSeqNum: 20,
+		serverSeqNum: 19,
+	},
+	{
+		name:         "append_eq",
+		pending:      []trackerUpdate{{numMessages: 43}},
+		clientSeqNum: 0,
+		serverSeqNum: 43,
+	},
+	{
+		name:         "append_lt",
+		pending:      []trackerUpdate{{numMessages: 43}},
+		clientSeqNum: 42,
+		serverSeqNum: 42,
+	},
+	{
+		name: "expunge_append",
+		pending: []trackerUpdate{
+			{expunge: 42},
+			{numMessages: 42},
+		},
+		clientSeqNum: 42,
+		serverSeqNum: 0,
+	},
+	{
+		name: "expunge_append",
+		pending: []trackerUpdate{
+			{expunge: 42},
+			{numMessages: 42},
+		},
+		clientSeqNum: 0,
+		serverSeqNum: 42,
+	},
+	{
+		name: "append_expunge",
+		pending: []trackerUpdate{
+			{numMessages: 43},
+			{expunge: 42},
+		},
+		clientSeqNum: 42,
+		serverSeqNum: 0,
+	},
+	{
+		name: "append_expunge",
+		pending: []trackerUpdate{
+			{numMessages: 43},
+			{expunge: 42},
+		},
+		clientSeqNum: 0,
+		serverSeqNum: 42,
+	},
+}
+
+func TestSessionTracker(t *testing.T) {
+	for _, tc := range sessionTrackerSeqNumTests {
+		tc := tc // capture range variable
+		t.Run(tc.name, func(t *testing.T) {
+			mboxTracker := imapserver.NewMailboxTracker(42)
+			sessTracker := mboxTracker.NewSession()
+			for _, update := range tc.pending {
+				switch {
+				case update.expunge != 0:
+					mboxTracker.QueueExpunge(update.expunge)
+				case update.numMessages != 0:
+					mboxTracker.QueueNumMessages(update.numMessages)
+				}
+			}
+
+			serverSeqNum := sessTracker.DecodeSeqNum(tc.clientSeqNum)
+			if tc.clientSeqNum != 0 && serverSeqNum != tc.serverSeqNum {
+				t.Errorf("DecodeSeqNum(%v): got %v, want %v", tc.clientSeqNum, serverSeqNum, tc.serverSeqNum)
+			}
+
+			clientSeqNum := sessTracker.EncodeSeqNum(tc.serverSeqNum)
+			if tc.serverSeqNum != 0 && clientSeqNum != tc.clientSeqNum {
+				t.Errorf("EncodeSeqNum(%v): got %v, want %v", tc.serverSeqNum, clientSeqNum, tc.clientSeqNum)
+			}
+		})
+	}
+}
