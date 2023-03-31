c948ec7457b555db47baca60df48839135e9dd2b
Simon Ser <contact@emersion.fr>
2023-03-31 12:18:05 +0200

    imapserver: announce IMAP4rev1 capabilities

--- imapserver/capability.go
+++ imapserver/capability.go
@@ -20,7 +20,8 @@ func (c *Conn) handleCapability(dec *imapwire.Decoder) error {
 }
 
 func (c *Conn) availableCaps() []imap.Cap {
-	caps := []imap.Cap{imap.CapIMAP4rev2}
+	caps := []imap.Cap{imap.CapIMAP4rev2, imap.CapIMAP4rev1}
+	caps = append(caps, imap4rev1BaseCaps...)
 	if c.canStartTLS() {
 		caps = append(caps, imap.CapStartTLS)
 	}
@@ -29,5 +30,27 @@ func (c *Conn) availableCaps() []imap.Cap {
 	} else if c.state == imap.ConnStateNotAuthenticated {
 		caps = append(caps, imap.CapLoginDisabled)
 	}
+	if c.state == imap.ConnStateAuthenticated || c.state == imap.ConnStateSelected {
+		caps = append(caps, imap4rev1AuthCaps...)
+	}
 	return caps
+}
+
+var imap4rev1BaseCaps = []imap.Cap{
+	imap.CapSASLIR,
+	imap.CapLiteralMinus,
+}
+
+var imap4rev1AuthCaps = []imap.Cap{
+	imap.CapNamespace,
+	imap.CapUnselect,
+	imap.CapUIDPlus,
+	imap.CapESearch,
+	// TODO: implement imap.CapSearchRes
+	imap.CapEnable,
+	imap.CapIdle,
+	imap.CapListExtended,
+	imap.CapListStatus,
+	imap.CapMove,
+	imap.CapStatusSize,
 }
