emersion/go-imap

commit c99745ccc613122f990f0e4e2e7ba89dacbb88c9

imapserver: allow CREATE-SPECIAL-USE to be advertised by the backend
Fixes: 66b34e4e5ecb ("Add support for CREATE-SPECIAL-USE")

imapserver/capability.go
@@ -73,6 +73,7 @@ func (c *Conn) availableCaps() []imap.Cap {
 				imap.CapListStatus,
 				imap.CapMove,
 				imap.CapStatusSize,
+				imap.CapCreateSpecialUse,
 			})
 		}
 	}

commit a74278f1134292b1c0376128466c953829b2d2fd

imapserver: allow CREATE-SPECIAL-USE to be advertised with IMAP4rev2
Fixes: c99745ccc613 ("imapserver: allow CREATE-SPECIAL-USE to be advertised by the backend")

imapserver/capability.go
@@ -73,9 +73,11 @@ func (c *Conn) availableCaps() []imap.Cap {
 				imap.CapListStatus,
 				imap.CapMove,
 				imap.CapStatusSize,
-				imap.CapCreateSpecialUse,
 			})
 		}
+		addAvailableCaps(&caps, available, []imap.Cap{
+			imap.CapCreateSpecialUse,
+		})
 	}
 	return caps
 }