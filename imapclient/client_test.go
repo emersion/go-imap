92547d6b90272d74f427bf7facdbc9152641de45
Simon Ser <contact@emersion.fr>
2023-04-16 20:43:48 +0200

    imapclient: add a few more examples

--- imapclient/client_test.go
+++ imapclient/client_test.go
@@ -1,6 +1,7 @@
 package imapclient_test
 
 import (
+	"io"
 	"log"
 
 	"github.com/emersion/go-imap/v2"
@@ -46,4 +47,122 @@ func ExampleClient() {
 	if err := c.Logout().Wait(); err != nil {
 		log.Fatalf("failed to logout: %v", err)
 	}
+}
+
+func ExampleClient_Append() {
+	var c *imapclient.Client
+
+	buf := []byte("From: <root@nsa.gov>\r\n\r\nHi <3")
+	size := int64(len(buf))
+	appendCmd := c.Append("INBOX", size, nil)
+	if _, err := appendCmd.Write(buf); err != nil {
+		log.Fatalf("failed to write message: %v", err)
+	}
+	if err := appendCmd.Close(); err != nil {
+		log.Fatalf("failed to close message: %v", err)
+	}
+	if _, err := appendCmd.Wait(); err != nil {
+		log.Fatalf("APPEND command failed: %v", err)
+	}
+}
+
+func ExampleClient_Status() {
+	var c *imapclient.Client
+
+	statusItems := []imap.StatusItem{imap.StatusItemNumMessages}
+	if data, err := c.Status("INBOX", statusItems).Wait(); err != nil {
+		log.Fatalf("STATUS command failed: %v", err)
+	} else {
+		log.Printf("INBOX contains %v messages", *data.NumMessages)
+	}
+}
+
+func ExampleClient_Store() {
+	var c *imapclient.Client
+
+	seqSet := imap.SeqSetNum(1)
+	storeFlags := imap.StoreFlags{
+		Op:     imap.StoreFlagsAdd,
+		Flags:  []imap.Flag{imap.FlagFlagged},
+		Silent: true,
+	}
+	if err := c.Store(seqSet, &storeFlags, nil).Close(); err != nil {
+		log.Fatalf("STORE command failed: %v", err)
+	}
+}
+
+func ExampleClient_Fetch() {
+	var c *imapclient.Client
+
+	seqSet := imap.SeqSetNum(1)
+	fetchItems := []imap.FetchItem{
+		imap.FetchItemFlags,
+		imap.FetchItemEnvelope,
+		&imap.FetchItemBodySection{Specifier: imap.PartSpecifierHeader},
+	}
+	messages, err := c.Fetch(seqSet, fetchItems, nil).Collect()
+	if err != nil {
+		log.Fatalf("FETCH command failed: %v", err)
+	}
+
+	msg := messages[0]
+	var header []byte
+	for _, buf := range msg.BodySection {
+		header = buf
+		break
+	}
+
+	log.Printf("Flags: %v", msg.Flags)
+	log.Printf("Subject: %v", msg.Envelope.Subject)
+	log.Printf("Header:\n%v", string(header))
+}
+
+func ExampleClient_Fetch_stream() {
+	var c *imapclient.Client
+
+	seqSet := imap.SeqSetNum(1)
+	fetchItems := []imap.FetchItem{
+		imap.FetchItemUID,
+		&imap.FetchItemBodySection{},
+	}
+	fetchCmd := c.Fetch(seqSet, fetchItems, nil)
+	for {
+		msg := fetchCmd.Next()
+		if msg == nil {
+			break
+		}
+
+		for {
+			item := msg.Next()
+			if item == nil {
+				break
+			}
+
+			switch item := item.(type) {
+			case imapclient.FetchItemDataUID:
+				log.Printf("UID: %v", item.UID)
+			case imapclient.FetchItemDataBodySection:
+				b, err := io.ReadAll(item.Literal)
+				if err != nil {
+					log.Fatalf("failed to read body section: %v", err)
+				}
+				log.Printf("Body:\n%v", string(b))
+			}
+		}
+	}
+	if err := fetchCmd.Close(); err != nil {
+		log.Fatalf("FETCH command failed: %v", err)
+	}
+}
+
+func ExampleClient_Search() {
+	var c *imapclient.Client
+
+	data, err := c.UIDSearch(&imap.SearchCriteria{
+		Body: []string{"Hello world"},
+	}, nil).Wait()
+	if err != nil {
+		log.Fatalf("UID SEARCH command failed: %v", err)
+	}
+	log.Fatalf("UIDs matching the search criteria: %v", data.AllNums())
 }
