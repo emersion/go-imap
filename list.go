1c804f1ec74d4c3c9721e5b4745933016ab475ee
Tim Culverhouse <tim@timculverhouse.com>
2023-08-07 13:59:58 -0500

    imapclient: add SPECIAL-USE support
    Servers are allowed to optionally send SPECIAL-USE mailbox attributes.
    Add support for clients to explicitly request that SPECIAL-USE
    attributes be sent.
    
    Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>

--- list.go
+++ list.go
@@ -5,10 +5,12 @@ type ListOptions struct {
 	SelectSubscribed     bool
 	SelectRemote         bool
 	SelectRecursiveMatch bool // requires SelectSubscribed to be set
+	SelectSpecialUse     bool // requires SPECIAL-USE
 
 	ReturnSubscribed bool
 	ReturnChildren   bool
 	ReturnStatus     *StatusOptions // requires IMAP4rev2 or LIST-STATUS
+	ReturnSpecialUse bool           // requires SPECIAL-USE
 }
 
 // ListData is the mailbox data returned by a LIST command.
