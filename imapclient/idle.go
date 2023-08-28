ab21f57fbbb6e0897dcec94c2ae0ea832ffe3883
Simon Ser <contact@emersion.fr>
2023-08-28 11:18:40 +0200

    imapclient: add IDLE fallback

--- imapclient/idle.go
+++ imapclient/idle.go
@@ -2,7 +2,12 @@ package imapclient
 
 import (
 	"fmt"
+	"time"
+
+	"github.com/emersion/go-imap/v2"
 )
+
+const idleFallbackInterval = time.Minute
 
 // Idle sends an IDLE command.
 //
@@ -11,8 +16,16 @@ import (
 // The caller must invoke IdleCommand.Close to stop IDLE and unblock the
 // client.
 //
-// This command requires support for IMAP4rev2 or the IDLE extension.
+// If the server lacks support for IMAP4rev2 or IDLE, this function falls back
+// to polling based on NOOP.
 func (c *Client) Idle() (*IdleCommand, error) {
+	if !c.Caps().Has(imap.CapIdle) {
+		if err := c.Noop().Wait(); err != nil {
+			return nil, err
+		}
+		return &IdleCommand{fallback: newIdleCommandFallback(c)}, nil
+	}
+
 	cmd := &IdleCommand{}
 	contReq := c.registerContReq(cmd)
 	cmd.enc = c.beginCommand("IDLE", cmd)
@@ -36,6 +49,8 @@ func (c *Client) Idle() (*IdleCommand, error) {
 type IdleCommand struct {
 	cmd
 	enc *commandEncoder
+
+	fallback *idleCommandFallback
 }
 
 // Close stops the IDLE command.
@@ -43,6 +58,11 @@ type IdleCommand struct {
 // This method blocks until the command to stop IDLE is written, but doesn't
 // wait for the server to respond. Callers can use Wait for this purpose.
 func (cmd *IdleCommand) Close() error {
+	if cmd.fallback != nil {
+		close(cmd.fallback.stop)
+		return nil
+	}
+
 	if cmd.err != nil {
 		return cmd.err
 	}
@@ -63,8 +83,48 @@ func (cmd *IdleCommand) Close() error {
 //
 // Wait can only be called after Close.
 func (cmd *IdleCommand) Wait() error {
+	if cmd.fallback != nil {
+		<-cmd.fallback.done
+		return cmd.fallback.err
+	}
+
 	if cmd.enc != nil {
 		return fmt.Errorf("imapclient: IdleCommand.Close must be called before Wait")
 	}
 	return cmd.cmd.Wait()
+}
+
+type idleCommandFallback struct {
+	client *Client
+	timer  *time.Timer
+	stop   chan struct{}
+	done   chan struct{}
+	err    error
+}
+
+func newIdleCommandFallback(c *Client) *idleCommandFallback {
+	return &idleCommandFallback{
+		client: c,
+		timer:  time.NewTimer(idleFallbackInterval),
+		stop:   make(chan struct{}),
+		done:   make(chan struct{}),
+	}
+}
+
+func (fallback *idleCommandFallback) run() {
+	defer close(fallback.done)
+	defer fallback.timer.Stop()
+
+	for {
+		select {
+		case <-fallback.timer.C:
+			if err := fallback.client.Noop().Wait(); err != nil {
+				fallback.err = err
+				return
+			}
+			fallback.timer.Reset(idleFallbackInterval)
+		case <-fallback.stop:
+			return
+		}
+	}
 }
