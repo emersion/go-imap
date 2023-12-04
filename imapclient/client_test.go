4ef8baf8298f4fbc7406797ad3ba1ea7fc903ec7
Simon Ser <contact@emersion.fr>
2023-12-04 12:53:15 +0100

    imapclient: add basic login test

---
+++ imapclient/client_test.go
@@ -0,0 +1,60 @@
+package imapclient_test
+
+import (
+	"io"
+	"net"
+	"testing"
+
+	"github.com/emersion/go-imap/v2/imapclient"
+	"github.com/emersion/go-imap/v2/imapserver"
+	"github.com/emersion/go-imap/v2/imapserver/imapmemserver"
+)
+
+const (
+	testUsername = "test-user"
+	testPassword = "test-password"
+)
+
+func newClientServerPair(t *testing.T) (*imapclient.Client, io.Closer) {
+	memServer := imapmemserver.New()
+
+	user := imapmemserver.NewUser(testUsername, testPassword)
+	user.Create("INBOX", nil)
+	memServer.AddUser(user)
+
+	server := imapserver.New(&imapserver.Options{
+		NewSession: func(conn *imapserver.Conn) (imapserver.Session, *imapserver.GreetingData, error) {
+			return memServer.NewSession(), nil, nil
+		},
+		InsecureAuth: true,
+	})
+
+	ln, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("net.Listen() = %v", err)
+	}
+
+	go func() {
+		if err := server.Serve(ln); err != nil {
+			t.Errorf("Serve() = %v", err)
+		}
+	}()
+
+	conn, err := net.Dial("tcp", ln.Addr().String())
+	if err != nil {
+		t.Fatalf("net.Dial() = %v", err)
+	}
+
+	client := imapclient.New(conn, nil)
+	return client, server
+}
+
+func TestLogin(t *testing.T) {
+	client, server := newClientServerPair(t)
+	defer client.Close()
+	defer server.Close()
+
+	if err := client.Login(testUsername, testPassword).Wait(); err != nil {
+		t.Errorf("Login().Wait() = %v", err)
+	}
+}
