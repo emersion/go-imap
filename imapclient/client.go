emersion/go-imap

commit 4be90aef12191eb2c344b2008d742ec4abf978a9

imapclient: Close returned err.NetClosed after Logout

imapclient/client.go
@@ -22,6 +22,7 @@ package imapclient
 import (
 	"bufio"
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"io"
 	"mime"
@@ -317,12 +318,12 @@ func (c *Client) Close() error {
 	c.mutex.Unlock()
 
 	// Ignore net.ErrClosed here, because we also call conn.Close in c.read
-	if err := c.conn.Close(); err != nil && err != net.ErrClosed {
+	if err := c.conn.Close(); err != nil && !errors.Is(err, net.ErrClosed) {
 		return err
 	}
 
 	<-c.decCh
-	if err := c.decErr; err != nil {
+	if err := c.decErr; err != nil && !errors.Is(err, net.ErrClosed) {
 		return err
 	}
 

imapclient/client_test.go
@@ -86,6 +86,18 @@ func TestLogin(t *testing.T) {
 	}
 }
 
+func TestLogout(t *testing.T) {
+	client, server := newClientServerPair(t, imap.ConnStateAuthenticated)
+	defer server.Close()
+
+	if err := client.Logout().Wait(); err != nil {
+		t.Errorf("Logout().Wait() = %v", err)
+	}
+	if err := client.Close(); err != nil {
+		t.Errorf("Close() = %v", err)
+	}
+}
+
 func TestIdle(t *testing.T) {
 	client, server := newClientServerPair(t, imap.ConnStateSelected)
 	defer client.Close()

commit cdca75fc64437fb336315c8b1a1e815a6772ddaf

imapclient: fix net.ErrClosed decoder check
Fixes: 4be90aef1219 ("imapclient: Close returned err.NetClosed after Logout")

imapclient/client.go
@@ -323,7 +323,7 @@ func (c *Client) Close() error {
 	}
 
 	<-c.decCh
-	if err := c.decErr; err != nil && !errors.Is(err, net.ErrClosed) {
+	if err := c.decErr; err != nil {
 		return err
 	}
 
@@ -519,7 +519,8 @@ func (c *Client) read() {
 
 	c.setReadTimeout(idleReadTimeout)
 	for {
-		if c.dec.EOF() {
+		// Ignore net.ErrClosed here, because we also call conn.Close in c.Close
+		if c.dec.EOF() || errors.Is(c.dec.Err(), net.ErrClosed) {
 			break
 		}
 		if err := c.readResponse(); err != nil {