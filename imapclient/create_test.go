13a0294b64093a8868d6d754fc22cded2bf9b62d
Simon Ser <contact@emersion.fr>
2024-04-15 13:33:06 +0200

    imapclient: add Create test with UTF-8 characters
    References: https://github.com/emersion/go-imap/issues/607

--- imapclient/create_test.go
+++ imapclient/create_test.go
@@ -6,12 +6,22 @@ import (
 	"github.com/emersion/go-imap/v2"
 )
 
-func TestCreate(t *testing.T) {
+func testCreate(t *testing.T, name string, utf8Accept bool) {
 	client, server := newClientServerPair(t, imap.ConnStateAuthenticated)
 	defer client.Close()
 	defer server.Close()
 
-	name := "Test mailbox"
+	if utf8Accept {
+		if !client.Caps().Has(imap.CapUTF8Accept) {
+			t.Skipf("missing UTF8=ACCEPT support")
+		}
+		if data, err := client.Enable(imap.CapUTF8Accept).Wait(); err != nil {
+			t.Fatalf("Enable(CapUTF8Accept) = %v", err)
+		} else if !data.Caps.Has(imap.CapUTF8Accept) {
+			t.Fatalf("server refused to enable UTF8=ACCEPT")
+		}
+	}
+
 	if err := client.Create(name, nil).Wait(); err != nil {
 		t.Fatalf("Create() = %v", err)
 	}
@@ -23,4 +33,25 @@ func TestCreate(t *testing.T) {
 	} else if len(mailboxes) != 1 || mailboxes[0].Mailbox != name {
 		t.Errorf("List() = %v, want exactly one entry with correct name", mailboxes)
 	}
+}
+
+func TestCreate(t *testing.T) {
+	t.Run("basic", func(t *testing.T) {
+		testCreate(t, "Test mailbox", false)
+	})
+
+	t.Run("unicode_utf7", func(t *testing.T) {
+		testCreate(t, "Cafè", false)
+	})
+	t.Run("unicode_utf8", func(t *testing.T) {
+		testCreate(t, "Cafè", true)
+	})
+
+	// '&' is the UTF-7 escape character
+	t.Run("ampersand_utf7", func(t *testing.T) {
+		testCreate(t, "Angus & Julia", false)
+	})
+	t.Run("ampersand_utf8", func(t *testing.T) {
+		testCreate(t, "Angus & Julia", true)
+	})
 }
