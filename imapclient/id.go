a440d45386f473d9edce23bfa2f4c863fa422dee
vzeroupper <vzeroupper>
2025-03-17 20:35:20 +0300

    rewrite id command according to rfc

--- imapclient/id.go
+++ imapclient/id.go
@@ -1,163 +1,225 @@
 package imapclient
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 
-	"github.com/emersion/go-imap/v2"
 	"github.com/emersion/go-imap/v2/internal/imapwire"
 )
 
-// ID sends an ID command.
-//
-// The ID command is introduced in RFC 2971. It requires support for the ID
-// extension.
-//
-// An example ID command:
-//
-//	ID ("name" "go-imap" "version" "1.0" "os" "Linux" "os-version" "7.9.4" "vendor" "Yahoo")
-func (c *Client) ID(idData *imap.IDData) *IDCommand {
-	cmd := &IDCommand{}
-	enc := c.beginCommand("ID", cmd)
+const (
+	idMaxFieldValuePairs = 30
+	idMaxFieldLength     = 30
+	idMaxValueLength     = 1024
+)
 
-	if idData == nil {
-		enc.SP().NIL()
-		enc.end()
-		return cmd
-	}
-
-	enc.SP().Special('(')
-	isFirstKey := true
-	if idData.Name != "" {
-		addIDKeyValue(enc, &isFirstKey, "name", idData.Name)
-	}
-	if idData.Version != "" {
-		addIDKeyValue(enc, &isFirstKey, "version", idData.Version)
-	}
-	if idData.OS != "" {
-		addIDKeyValue(enc, &isFirstKey, "os", idData.OS)
-	}
-	if idData.OSVersion != "" {
-		addIDKeyValue(enc, &isFirstKey, "os-version", idData.OSVersion)
-	}
-	if idData.Vendor != "" {
-		addIDKeyValue(enc, &isFirstKey, "vendor", idData.Vendor)
-	}
-	if idData.SupportURL != "" {
-		addIDKeyValue(enc, &isFirstKey, "support-url", idData.SupportURL)
-	}
-	if idData.Address != "" {
-		addIDKeyValue(enc, &isFirstKey, "address", idData.Address)
-	}
-	if idData.Date != "" {
-		addIDKeyValue(enc, &isFirstKey, "date", idData.Date)
-	}
-	if idData.Command != "" {
-		addIDKeyValue(enc, &isFirstKey, "command", idData.Command)
-	}
-	if idData.Arguments != "" {
-		addIDKeyValue(enc, &isFirstKey, "arguments", idData.Arguments)
-	}
-	if idData.Environment != "" {
-		addIDKeyValue(enc, &isFirstKey, "environment", idData.Environment)
-	}
-
-	enc.Special(')')
-	enc.end()
-	return cmd
+type IDCommand struct {
+	commandBase
+	data map[string]*string
+	err  error
 }
 
-func addIDKeyValue(enc *commandEncoder, isFirstKey *bool, key, value string) {
-	if isFirstKey == nil {
-		panic("isFirstKey cannot be nil")
-	} else if !*isFirstKey {
-		enc.SP().Quoted(key).SP().Quoted(value)
-	} else {
-		enc.Quoted(key).SP().Quoted(value)
+func (command *IDCommand) Wait() (map[string]*string, error) {
+	if command.err != nil {
+		return nil, command.err
 	}
-	*isFirstKey = false
+	err := command.wait()
+	return command.data, err
 }
 
-func (c *Client) handleID() error {
-	data, err := c.readID(c.dec)
-	if err != nil {
-		return fmt.Errorf("in id: %v", err)
+func validateIDPayload(fields map[string]*string) error {
+	if len(fields) > idMaxFieldValuePairs {
+		return fmt.Errorf(
+			"cannot send %d field-value pairs as only up to %d are allowed",
+			len(fields),
+			idMaxFieldValuePairs)
 	}
 
-	if cmd := findPendingCmdByType[*IDCommand](c); cmd != nil {
-		cmd.data = *data
+	fieldsSeen := map[string]bool{}
+
+	for field, value := range fields {
+		if len(field) > idMaxFieldLength {
+			return fmt.Errorf(
+				"cannot send a %d-octet field as fields only up to %d octets are allowed",
+				len(field),
+				idMaxFieldLength)
+		}
+		if value != nil && len(*value) > idMaxValueLength {
+			return fmt.Errorf(
+				"cannot send a %d-octet value as values only up to %d octets are allowed",
+				len(*value),
+				idMaxValueLength)
+		}
+
+		fieldLowered := strings.ToLower(field)
+		if fieldsSeen[fieldLowered] {
+			return fmt.Errorf("cannot send field %q twice", field)
+		}
+		fieldsSeen[fieldLowered] = true
 	}
 
 	return nil
 }
 
-func (c *Client) readID(dec *imapwire.Decoder) (*imap.IDData, error) {
-	var data = imap.IDData{}
-
-	if !dec.ExpectSP() {
-		return nil, dec.Err()
+// ID sends an ID command.
+//
+// This command requires support for the ID extension (RFC 2971).
+//
+// Fields (map keys) are case-insensitive in both requests and responses.
+// Fields' case in requests might not be preserved. Fields are brought to
+// lower case in responses.
+//
+// NIL lists are presented as nil maps, empty lists are presented as empty maps,
+// sent or received.
+func (client *Client) ID(fields map[string]*string) *IDCommand {
+	if err := validateIDPayload(fields); err != nil {
+		// Remain consistent with *imap.Error being the only non-fatal error
+		client.closeWithError(err)
+		return &IDCommand{err: err}
 	}
 
-	if dec.ExpectNIL() {
-		return &data, nil
+	command := &IDCommand{}
+	encoder := client.beginCommand("ID", command)
+	encoder.SP()
+
+	if fields == nil {
+		encoder.NIL()
+		encoder.end()
+		return command
 	}
 
-	currKey := ""
-	err := dec.ExpectList(func() error {
-		var keyOrValue string
-		if !dec.String(&keyOrValue) {
-			return fmt.Errorf("in id key-val list: %v", dec.Err())
+	list := encoder.BeginList()
+	for field, value := range fields {
+		list.Item().String(field)
+		if value != nil {
+			list.Item().String(*value)
+		} else {
+			list.Item().NIL()
+		}
+	}
+	list.End()
+
+	encoder.end()
+	return command
+}
+
+// Attempt to read NIL for an optional value that can't be an atom and doesn't start with an
+// ATOM-CHAR. If a non-NIL atom is scanned, an error is set on decoder.
+func tryReadingNil(decoder *imapwire.Decoder) bool {
+	var atom string
+	return decoder.Atom(&atom) && decoder.Expect(atom == "NIL", "NIL")
+}
+
+func fieldReader(decoder *imapwire.Decoder, result map[string]*string) func() (string, error) {
+	return func() (string, error) {
+		field := ""
+		if !decoder.ExpectString(&field) {
+			return "", decoder.Err()
 		}
 
-		if currKey == "" {
-			currKey = keyOrValue
+		if len(field) > idMaxFieldLength {
+			return "", fmt.Errorf(
+				"illegal %d-octet field as fields only up to %d octets are allowed",
+				len(field),
+				idMaxFieldLength)
+		}
+
+		fieldLowered := strings.ToLower(field)
+		if _, found := result[fieldLowered]; found {
+			return "", fmt.Errorf("received field %q twice", field)
+		}
+
+		if len(result) == idMaxFieldValuePairs {
+			return "", errors.New("too many field-value pairs")
+		}
+
+		return fieldLowered, nil
+	}
+}
+
+func valueReader(decoder *imapwire.Decoder, result map[string]*string) func(string) error {
+	return func(key string) error {
+		if tryReadingNil(decoder) {
+			result[key] = nil
 			return nil
 		}
 
-		switch currKey {
-		case "name":
-			data.Name = keyOrValue
-		case "version":
-			data.Version = keyOrValue
-		case "os":
-			data.OS = keyOrValue
-		case "os-version":
-			data.OSVersion = keyOrValue
-		case "vendor":
-			data.Vendor = keyOrValue
-		case "support-url":
-			data.SupportURL = keyOrValue
-		case "address":
-			data.Address = keyOrValue
-		case "date":
-			data.Date = keyOrValue
-		case "command":
-			data.Command = keyOrValue
-		case "arguments":
-			data.Arguments = keyOrValue
-		case "environment":
-			data.Environment = keyOrValue
-		default:
-			// Ignore unknown key
-			// Yahoo server sends "host" and "remote-host" keys
-			// which are not defined in RFC 2971
+		value := ""
+		if !decoder.ExpectString(&value) {
+			return decoder.Err()
 		}
-		currKey = ""
 
+		if len(value) > idMaxValueLength {
+			return fmt.Errorf(
+				"illegal %d-octet value as values only up to %d octets are allowed",
+				len(value),
+				idMaxValueLength)
+		}
+
+		result[key] = &value
 		return nil
-	})
+	}
+}
 
+func expectMap(decoder *imapwire.Decoder, onKey func() error, onValue func() error) error {
+	seenKey := false
+
+	err := decoder.ExpectList(func() error {
+		if seenKey {
+			seenKey = false
+			return onValue()
+		}
+		seenKey = true
+		return onKey()
+	})
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return &data, nil
+	if seenKey {
+		return errors.New("uneven number of elements in list")
+	}
+	return nil
 }
 
-type IDCommand struct {
-	commandBase
-	data imap.IDData
+func readIDResponse(decoder *imapwire.Decoder) (map[string]*string, error) {
+	if !decoder.ExpectSP() {
+		return nil, decoder.Err()
+	}
+	if tryReadingNil(decoder) {
+		return nil, nil
+	}
+	if decoder.Err() != nil {
+		return nil, decoder.Err()
+	}
+
+	result := map[string]*string{}
+	readField := fieldReader(decoder, result)
+	readValue := valueReader(decoder, result)
+	field := ""
+
+	err := expectMap(decoder,
+		func() (err error) {
+			field, err = readField()
+			return
+		},
+		func() error {
+			return readValue(field)
+		})
+
+	return result, err
 }
 
-func (r *IDCommand) Wait() (*imap.IDData, error) {
-	return &r.data, r.wait()
+func (client *Client) handleID() error {
+	response, err := readIDResponse(client.dec)
+	if err != nil {
+		return fmt.Errorf("in id: %v", err)
+	}
+
+	if cmd := findPendingCmdByType[*IDCommand](client); cmd != nil {
+		cmd.data = response
+	}
+
+	return nil
 }
