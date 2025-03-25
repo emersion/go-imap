17424b3d1996ecf705675d23cd0e9dbf24d35481
hutiquan <xiaoquanidea@163.com>
2025-03-25 19:35:24 +0800

    fix issue #659, compatible attachment description """" parsing.

--- imapclient/fetch.go
+++ imapclient/fetch.go
@@ -970,7 +970,7 @@ func readBodyType1part(dec *imapwire.Decoder, typ string, options *Options) (*im
 	}
 
 	var description string
-	if !dec.ExpectSP() || !dec.ExpectNString(&bs.ID) || !dec.ExpectSP() || !dec.ExpectNString(&description) || !dec.ExpectSP() || !dec.ExpectNString(&bs.Encoding) || !dec.ExpectSP() || !dec.ExpectBodyFldOctets(&bs.Size) {
+	if !dec.ExpectSP() || !dec.ExpectNString(&bs.ID) || !dec.ExpectSP() || !dec.ExpectNStringWithDoubleQuoteCompat(&description) || !dec.ExpectSP() || !dec.ExpectNString(&bs.Encoding) || !dec.ExpectSP() || !dec.ExpectBodyFldOctets(&bs.Size) {
 		return nil, dec.Err()
 	}
 
